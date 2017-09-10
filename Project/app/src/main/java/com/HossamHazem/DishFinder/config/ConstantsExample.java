8e7c638b999e7ec26f7678934d9cf13922cb8123
Hossam Hazem <hossamhazem94@gmail.com>
2017-09-10 23:12:10 +0200

    remove the forced error

--- Project/app/src/main/java/com/HossamHazem/DishFinder/config/ConstantsExample.java
+++ Project/app/src/main/java/com/HossamHazem/DishFinder/config/ConstantsExample.java
@@ -1,5 +1,5 @@
 package com.HossamHazem.DishFinder.config;
 
 public class ConstantsExample {
-    public static final String GOOGLE_PLACES_API_KEY = Your Key;
+    public static final String GOOGLE_PLACES_API_KEY = "Your Key";
 }
